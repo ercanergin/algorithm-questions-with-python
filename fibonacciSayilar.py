f24f61478de64df740034aed008c7f7dc100546b
Ercan ERGİN <ercanergin623@gmail.com>
2017-01-16 14:54:38 +0200

    Add files via upload

---
+++ fibonacciSayilar.py
@@ -0,0 +1,8 @@
+# Dört milyondan küçük çift Fibonacci sayılarının toplamını bulan program
+a,b,c,toplam=1,1,0,0
+while c<4000000:
+    if c%2==0:
+        toplam=toplam+c
+    c=a+b
+    a,b=b,c
+print(toplam)
